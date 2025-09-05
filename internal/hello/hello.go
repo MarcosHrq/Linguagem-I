8198a87127277a228c8f14495137759149f09d65
Marcos Henrique Mesquita <marcos.hennrrique@gmail.com>
2025-09-05 19:47:35 -0300

    atividade 05set2025/primiera etapa

--- internal/hello/hello.go
+++ internal/hello/hello.go
@@ -8,5 +8,5 @@ import "fmt"
 // Função simples para demonstrar organização do código
 func SayHello() {
     // Imprime uma mensagem no console
-    fmt.Println("Olá, mundo! FacINpro! 👋")
+    fmt.Println("Olá, mundo! FacINpro! Marcos Henrique da S. A. Mesquita 👋")
 }

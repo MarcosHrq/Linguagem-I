5ed7097fb044f3089f6cf6660f4f06dcbdbe7413
MarcosHrq <marcos.hennrrique@gmail.com>
2025-09-07 20:04:16 -0300

    parte 2 atividade

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -2,14 +2,19 @@
 // Convenção de mercado: colocar em cmd/<nome-app>/main.go
 package main
 
-// Importa os pacotes necessários 
+// Importa os pacotes necessários
 import (
-    "fmt"
-    "github.com/seu-usuario/meu-projeto-go/internal/hello"
+	"fmt"
+
+	"github.com/seu-usuario/meu-projeto-go/internal/fibonacci"
+	"github.com/seu-usuario/meu-projeto-go/internal/hello"
 )
 
 // Função principal do programa
 func main() {
-    fmt.Println("🚀 Meu primeiro projeto em Go com estrutura de mercado!")
-    hello.SayHello()
+	fmt.Println("🚀 Meu primeiro projeto em Go com estrutura de mercado!")
+	hello.SayHello()
+	n := 10
+	fmt.Println("O", n, "º número de Fibonacci é:", fibonacci.Fibonacci(n))
+
 }
